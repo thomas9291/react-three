bcd42000b5fab17ba6a181875ae96be6c917cc0e
thomas9291 <thomas.jubin92@gmil.com>
2024-04-22 09:08:22 +0200

    camera effecct added

--- src/pages/alien/index.js
+++ src/pages/alien/index.js
@@ -9,7 +9,7 @@ const index = () => {
       <h2>Alien</h2>
       <Link href={"/"}>back to the home page</Link>
       <div className="containerCanvas">
-        <Canvas shadows camera={{position:[0,4,12]}}>
+        <Canvas shadows camera={{position:[-2,30,-42]}}>
            
           <AlienComponent />
         </Canvas>
