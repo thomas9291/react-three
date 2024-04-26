b3f8ab16cc602b0165a27e87976c38011652de16
thomas9291 <thomas.jubin92@gmil.com>
2024-04-26 08:08:06 +0200

    sun ligth run

--- src/components/godRaysComponents/effectGodRays.jsx
+++ src/components/godRaysComponents/effectGodRays.jsx
@@ -1,17 +1,18 @@
-import React, { forwardRef, useEffect } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { EffectComposer, GodRays } from "@react-three/postprocessing";
 
 const EffectGodRays = forwardRef(function EffectGodRays(props, ref) {
+  const [isSun, setIsSun] = useState();
   useEffect(() => {
-    console.log("ref", ref);
-    console.log("ref current", ref.current);
+    setIsSun(ref.current)
+   
   }, [ref]);
   //il faut faire lapel du sun conditionelement parsque au premier render le sun est undefine,ces pour sa quon utilise useEffect
   return (
     <EffectComposer>
       {ref.current && (
         <GodRays
-          sun={ref}
+          sun={isSun}
           samples={60} // The number of samples per pixel.
           density={0.4} // The density of the light rays.
         />
